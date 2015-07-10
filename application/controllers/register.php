e9caac5ac73336baad5253ac1e114a5e297800c9
developertr <iletisim@sezginaltinoz.com>
2015-07-10 16:23:21 +0300

    register

---
+++ application/controllers/register.php
@@ -0,0 +1,10 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Register extends CI_Controller {
+
+	public function index()
+	{            
+        $datas["login"]=$this->session->userdata('login');
+        $this->load->view('eatsView',$datas);
+	}
+}
