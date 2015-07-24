8e71f354a43d06095c6c16822d4b557008e83097
cmdlmn <cem@Busy-2.local>
2015-07-24 19:02:43 +0300

    order_ok order_error

---
+++ application/views/order_error.php
@@ -0,0 +1,23 @@
+<?php $this->load->view('includes/head') ?>
+
+<script src="<?php echo base_url('assets/js/bottle_add.js') ?>"></script>
+
+</head>
+<body>
+<?php $this->load->view('includes/header') ?>
+        
+<div class="container">
+
+	<div class="order_status">
+    	<img src="assets/images/order_error.png" />
+        <span>ÖDEMENİZ GERÇEKLEŞMEDİ!</span>
+        <div>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+Lorem Ipsum has been the industry's standard dummy text eversince the 1500s, when an
+unknown printer took a galley of type and scrambled it
+		</div>
+    </div>   
+
+</div>
+<?php $this->load->view('includes/footer') ?>
+</body>
+</html>
---
+++ application/views/order_ok.php
@@ -0,0 +1,19 @@
+<?php $this->load->view('includes/head') ?>
+
+<script src="<?php echo base_url('assets/js/bottle_add.js') ?>"></script>
+
+</head>
+<body>
+<?php $this->load->view('includes/header') ?>
+        
+<div class="container">
+
+	<div class="order_status">
+    	<img src="assets/images/order_ok.png" />
+        <span>SİPARİŞİNİZ ONAYLANDI</span>
+    </div>   
+
+</div>
+<?php $this->load->view('includes/footer') ?>
+</body>
+</html>
